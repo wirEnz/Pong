95b9657196a069df25de4cc3198c945aa06f8e16
wirEnz <fps.andrew@gmail.com>
2019-04-12 17:31:24 +1200

    Set final static information.

--- Pong.java
+++ Pong.java
@@ -1,9 +1,25 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame {
+	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
 
+	public Pong () {
+			
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+				
+	}
+	
+	
 	public static void main(String[] args) {
 		
-		
+		new Pong();
 		
 
 	}
