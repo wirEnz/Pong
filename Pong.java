2ff34ec4fda68160bee50eed388a1407acfd7d07
wirEnz <fps.andrew@gmail.com>
2019-04-12 17:48:45 +1200

    Added pongPanel class, and added objectAid diagram

--- Pong.java
+++ Pong.java
@@ -2,17 +2,21 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 	
+	// private final static, cannot be changed once application starts
 	private final static String WINDOW_TITLE = "Pong";
 	private final static int WINDOW_WIDTH = 800;
 	private final static int WINDOW_HEIGHT = 600;
 
-	public Pong () {
+	public Pong() {
 			
+		// meaning in the name		
 		setTitle(WINDOW_TITLE);
 		setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		add(new PongPanel());
 				
 	}
 	
---
+++ PongPanel.java
@@ -0,0 +1,42 @@
+import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+
+	public PongPanel() {
+		setBackground(Color.BLACK);
+	}
+	
+	
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
